a95f50f4959c642877add96b51fb99da56b56758
Dmitry Perepelov <dperepelov@logicstars.ru>
2023-06-22 22:28:44 +0300

    js introduction

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,19 @@
+﻿let userInfo = new Map();
+debugger
+userInfo.set('User-Agent', window.navigator.userAgent);
+
+var age = prompt('Здрвастуйте! Пожалуйста, укажите Ваш возраст:');
+userInfo.set('Age', age);
+if (age >= 18) {
+    let dateTime = new Date().toLocaleString();
+    alert('Приветствуем на LifeSpot!\nТекущее время: ' + dateTime);
+    userInfo.set('TimeStart', dateTime);
+}
+else {
+    alert('Сайт LifeSpor.ru предназначен для лиц старше 18 лет!\nВы будете перенаправлены на страницу google.com');
+    window.location.href = 'https://google.com';
+}
+
+for (let elem of userInfo) {
+    console.log(elem);
+}
---
+++ LifeSpot/Static/JS/testing.js
@@ -0,0 +1,5 @@
+﻿let userName = prompt("Пожалуйста, введите Ваше имя:");
+alert(`Здравствуйте, ${userName} в вашем имение ${userName.length} символов.`);
+
+let elements = document.getElementsByTagName('*').length;
+alert(`Количество html элементов: ${elements}`);
