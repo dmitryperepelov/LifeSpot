b697d96838c61487716b68377220879019b80ef5
Dmitry Perepelov <dperepelov@logicstars.ru>
2023-06-27 07:38:57 +0300

    add about page

--- LifeSpot/Startup.cs
+++ LifeSpot/Startup.cs
@@ -32,6 +32,9 @@ namespace LifeSpot
 
             app.UseEndpoints(endpoints =>
             {
+
+                #region HTML
+
                 endpoints.MapGet("/", async context =>
                 {
                     var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "index.html");
@@ -56,12 +59,32 @@ namespace LifeSpot
                     await context.Response.WriteAsync(html.ToString());
                 });
 
+                endpoints.MapGet("/about", async context =>
+                {
+                    var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "about.html");
+
+                    // Загружаем шаблон страницы, вставляя в него элементы
+                    var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
+                        .Replace("<!--SIDEBAR-->", sideBarHtml)
+                        .Replace("<!--FOOTER-->", footerHtml);
+
+                    await context.Response.WriteAsync(html.ToString());
+                });
+
+                #endregion
+
+                #region CSS
+
                 endpoints.MapGet("/Static/CSS/index.css", async context =>
                 {
                     var cssPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "CSS", "index.css");
                     var css = await File.ReadAllTextAsync(cssPath);
                     await context.Response.WriteAsync(css);
                 });
+
+                #endregion
+
+                #region JavaScript
 
                 endpoints.MapGet("/Static/JS/index.js", async context =>
                 {
@@ -75,6 +98,14 @@ namespace LifeSpot
                     var js = await File.ReadAllTextAsync(jsPath);
                     await context.Response.WriteAsync(js);
                 });
+                endpoints.MapGet("/Static/JS/about.js", async context =>
+                {
+                    var jsPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "about.js");
+                    var js = await File.ReadAllTextAsync(jsPath);
+                    await context.Response.WriteAsync(js);
+                });
+
+                #endregion
             });
         }
     }
--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,19 +1,55 @@
-﻿let userInfo = new Map();
-debugger
-userInfo.set('User-Agent', window.navigator.userAgent);
+﻿// создадим объект Map для хранения сессии
+let session = new Map();
 
-var age = prompt('Здрвастуйте! Пожалуйста, укажите Ваш возраст:');
-userInfo.set('Age', age);
-if (age >= 18) {
-    let dateTime = new Date().toLocaleString();
-    alert('Приветствуем на LifeSpot!\nТекущее время: ' + dateTime);
-    userInfo.set('TimeStart', dateTime);
-}
-else {
-    alert('Сайт LifeSpor.ru предназначен для лиц старше 18 лет!\nВы будете перенаправлены на страницу google.com');
-    window.location.href = 'https://google.com';
+(function () {
+    setTimeout(() =>
+        alert("Нравится LifeSpot? " + '\n' + "Подпишитесь на наш Instagram @lifespot999!"),
+        30000);
+})();
+
+
+function logSession() {
+    for (let result of session) {
+        console.log(result)
+    }
 }
 
-for (let elem of userInfo) {
-    console.log(elem);
+function checkAge() {
+    // Запросим возраст пользователя и тоже сохраним
+    session.set("age", prompt("Пожалуйста, введите ваш возраст"))
+
+    // Проверка на возраст и сохранение сессии
+    if (session.get("age") >= 18) {
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
 }
+
+function handleSession() {
+
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent);
+    session.set("startDate", new Date().toLocaleString());
+    
+    return session;
+}
+
+function getUserInput() {
+    let userInput = document.getElementsByTagName('input')[0].value.toLowerCase();
+    return userInput;
+}
+
+function filterContent() {
+    let elements = document.getElementsByClassName('video-container');
+    for (let i = 0; i <= elements.length; i++) {
+        let childElements = elements[i];
+        let videoDescription = childElements.getElementsByTagName('h3')[0].innerText;
+        if (videoDescription.toLowerCase().includes(getUserInput()))
+            elements[i].style.display = 'inline-block';
+        else
+            elements[i].style.display = 'none';
+    }
+};
